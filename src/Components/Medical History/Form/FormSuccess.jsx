b26347ca858130ea4d25ab42e5874dd10a187d45
TomasCapizzi <tomas.capizzi@hotmail.com>
2023-08-07 21:51:24 -0300

    Change styles to Tailwind

--- src/Components/Medical History/Form/FormSuccess.jsx
+++ src/Components/Medical History/Form/FormSuccess.jsx
@@ -4,9 +4,9 @@ import React from 'react';
 
 function FormSuccess() {
   return (
-    <article className='form-success'>
-        <h5>Medical record stored</h5>
-        <button><Link to='/'>Go back <IoArrowBack/> </Link></button>
+    <article className='w-full flex justify-center flex-col items-center'>
+        <h5 className='text-lg text-cyan-600'>Medical record stored</h5>
+        <button className='bg-cyan-100 p-4 rounded-2xl w-32 border-none'><Link to='/'>Go back <IoArrowBack/> </Link></button>
     </article>
   )
 }
