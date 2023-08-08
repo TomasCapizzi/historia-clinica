a96047a1be7f6c4183ce3effd8c6eef42a0837c1
TomasCapizzi <tomas.capizzi@hotmail.com>
2023-08-07 21:49:31 -0300

    Change Spinner

--- src/Components/Spinner.jsx
+++ src/Components/Spinner.jsx
@@ -2,19 +2,9 @@ import React from "react";
 
 export default function Spinner(){
     return (
-        <div className="sk-circle">
-            <div className="sk-circle1 sk-child"></div>
-            <div className="sk-circle2 sk-child"></div>
-            <div className="sk-circle3 sk-child"></div>
-            <div className="sk-circle4 sk-child"></div>
-            <div className="sk-circle5 sk-child"></div>
-            <div className="sk-circle6 sk-child"></div>
-            <div className="sk-circle7 sk-child"></div>
-            <div className="sk-circle8 sk-child"></div>
-            <div className="sk-circle9 sk-child"></div>
-            <div className="sk-circle10 sk-child"></div>
-            <div className="sk-circle11 sk-child"></div>
-            <div className="sk-circle12 sk-child"></div>
+        <div class="spinner">
+            <div class="double-bounce1"></div>
+            <div class="double-bounce2"></div>
         </div>
     )
 }
